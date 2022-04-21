f6f8abf4a880bb415435d1d57093f757c75b84f2
Aprodeo Subarno <aprodeosubarno@gmail.com>
2022-04-21 12:43:16 +0700

    update: except create method from TodoController

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -21,16 +21,6 @@ class TodoController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
