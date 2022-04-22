528fb8c71cb9f5741afb6ebd3ea70c9f7cc76e6c
Aprodeo Subarno <aprodeosubarno@gmail.com>
2022-04-22 08:54:18 +0700

    add: add meta tag for description

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,6 +5,7 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Simple todolist with some features">
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
