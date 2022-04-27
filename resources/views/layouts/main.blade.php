870c8d10d292c69c6dc868f4982c4324a979f38d
Aprodeo Subarno <aprodeosubarno@gmail.com>
2022-04-27 08:52:47 +0700

    update: add icon for window browser in todos page

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -11,6 +11,9 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
+    {{-- Icon --}}
+    <link rel="icon" href="/icon/todo.png">
+
     {{-- Font --}}
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
