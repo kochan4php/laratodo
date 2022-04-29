eb59b111772afca21774edc6dbabef4dce30c1b9
Aprodeo Subarno <aprodeosubarno@gmail.com>
2022-04-29 15:07:44 +0700

    update: add commentar for route

--- routes/web.php
+++ routes/web.php
@@ -17,18 +17,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Todos Route
 Route::get('/', [TodoController::class, 'index'])->middleware('auth');
 Route::resource('/tasks', TodoController::class)->except(['show', 'create', 'edit'])->middleware('auth');
 Route::get('/completed', [TodoController::class, 'completed'])->middleware('auth');
 Route::get('/uncompleted', [TodoController::class, 'uncompleted'])->middleware('auth');
 
+// Register Route
 Route::get('/register', [RegisterController::class, 'index'])->middleware('guest');
 Route::post('/register', [RegisterController::class, 'register']);
 
+// Login Route
 Route::get('/login', [LoginController::class, 'index'])->name('login')->middleware('guest');
 Route::post('/login', [LoginController::class, 'login_authenticate']);
 Route::post('/logout', [LoginController::class, 'logout']);
 
+// Profile Route
 Route::get('/profile/{user:slug}', [ProfileController::class, 'index'])->middleware('auth');
 Route::get('/profile/edit/{user:slug}', [ProfileController::class, 'edit_profile'])->middleware('auth');
 Route::patch('/profile/{user:slug}', [ProfileController::class, 'update_profile']);
